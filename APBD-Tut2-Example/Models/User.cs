544ea9742b19d914c4723c41bec845afbc182855
Jarosław Trinh <s27521@pjwstk.edu.pl>
2026-03-24 23:20:06 +0100

    Added penalty list for users

--- APBD-Tut2-Example/Models/User.cs
+++ APBD-Tut2-Example/Models/User.cs
@@ -7,6 +7,7 @@ public abstract class User(string fName, string lName)
     public int Id { get; set; } = _nextId++;
     public string FName { get; set; } = fName;
     public string LName { get; set; } = lName;
+    public List<Penalty> Penalties = [];
     
     public abstract int GetMaxReservations();
 }
