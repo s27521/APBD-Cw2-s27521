c99ac73f0850326264f124888bee6b755b02d750
Jarosław Trinh <s27521@pjwstk.edu.pl>
2026-03-24 22:40:47 +0100

    Added Penalty class

---
+++ APBD-Tut2-Example/Models/Penalty.cs
@@ -0,0 +1,13 @@
+﻿namespace APBD_Tut2_Example.Models;
+
+public class Penalty(Reservation reservation, DateTime date)
+{
+    public Reservation Reservation { get; set; } = reservation;
+
+    public int PenaltyClass { get; set; } = (date - reservation.To).Days switch
+    {
+        < 3 => 1,
+        <=7 => 2,
+        > 7 => 3
+    };
+}
