37248a8fd59a8d72f91bb1083c73c70beea1b2f7
Jarosław Trinh <s27521@pjwstk.edu.pl>
2026-03-25 11:42:21 +0100

    Added Tests for the program

--- APBD-Tut2-Example/Program.cs
+++ APBD-Tut2-Example/Program.cs
@@ -6,19 +6,40 @@ using APBD_Tut2_Example.Services.Rooms;
 var user1 = new Employee("Jan", "Kowalski");
 var user2 = new Student("Michael", "Doe");
 
-var room1 = new Laptop("112B","Asus", true, "Ubuntu");
-var room2 = new Laptop("112C", "Lenovo", true, "Windows");
-var room3 = new Projector("112","Samsung", true, "Laser");
+var item1 = new Laptop("112B","Asus", true, "Ubuntu");
+var item2 = new Laptop("112C", "Lenovo", true, "Windows");
+var item3 = new Projector("112","Samsung", true, "Laser");
+var item4 = new Camera("asdjwad", "Canon", "CMOD", "20mpx");
 
 IEquipmentService equipmentService = new EquipmentService();
 
-equipmentService.AddEquipment(room1);
-equipmentService.AddEquipment(room2);
-equipmentService.AddEquipment(room3);
+equipmentService.AddEquipment(item1);
+equipmentService.AddEquipment(item2);
+equipmentService.AddEquipment(item3);
+equipmentService.AddEquipment(item4);
 
-equipmentService.SetUnavailable(room2.Id);
+equipmentService.SetUnavailable(item2.Id);
 
 IReservationService reservationService = new ReservationService();
+
+// Correct reservation making
+try
+{
+    reservationService.CreateReservation(
+        user2,
+        item1,
+        new DateTime(2026, 1, 1, 10, 0, 0),
+        new DateTime(2026, 4, 1, 11, 30, 0));
+    reservationService.CreateReservation(
+        user2,
+        item4,
+        new DateTime(2026, 1, 1, 10, 0, 0),
+        new DateTime(2026, 3, 1, 11, 30, 0));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
 
 // Attempt to book a room that is not available
 try
@@ -26,7 +47,7 @@ try
     Console.WriteLine("\n[Attempt to book a room that is not available]: ");
     reservationService.CreateReservation(
         user1,
-        room2,
+        item2,
         new DateTime(2026, 1, 1, 10, 0, 0),
         new DateTime(2026, 1, 1, 11, 30, 0));
 }
@@ -46,7 +67,7 @@ try
         new DateTime(2026, 1, 1, 11, 30, 0));
     reservationService.CreateReservation(
         user1,
-        room1,
+        item1,
         new DateTime(2026, 1, 1, 10, 0, 0),
         new DateTime(2026, 1, 1, 11, 30, 0));
 }
@@ -54,6 +75,9 @@ catch (Exception e)
 {
     Console.WriteLine(e.Message);
 }
+
+Console.WriteLine("\n" + equipmentService.GetStatus());
+Console.WriteLine(reservationService.GetStatus());
 
 // Attempt to cancel not existing reservation
 try
@@ -76,3 +100,31 @@ catch(Exception e)
 {
     Console.WriteLine(e.Message);
 }
+
+reservationService.GetUserReservations(user2).ForEach(p => Console.WriteLine(p.Id));
+// Correct return
+try
+{
+    reservationService.ReturnReservation(1, DateTime.Now);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+reservationService.GetUserReservations(user2).ForEach(p => Console.WriteLine(p.Id));
+Console.WriteLine("\nUser penalties: " + user2.Penalties.Count);
+
+// Return past due
+try
+{
+    reservationService.ReturnReservation(2, DateTime.Now);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+reservationService.GetUserReservations(user2).ForEach(p => Console.WriteLine(p.Id));
+Console.WriteLine("\nUser penalties: " + user2.Penalties.Count);
+
+Console.WriteLine("\n" + equipmentService.GetStatus());
+Console.WriteLine(reservationService.GetStatus());
